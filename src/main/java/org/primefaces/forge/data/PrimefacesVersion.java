6e08a495ad3a6dea562386fb6eb4a7c56dc42647
Rudy De Busscher <rdebusscher@gmail.com>
2012-01-11 14:29:31 +0100

    Updated to PrimeFaces 3.0 Final

--- src/main/java/org/primefaces/forge/data/PrimefacesVersion.java
+++ src/main/java/org/primefaces/forge/data/PrimefacesVersion.java
@@ -32,8 +32,8 @@ import java.util.List;
 public enum PrimefacesVersion {
     PRIMEFACES_2_2_1("Primefaces 2.2.1", Arrays.asList(DependencyBuilder.create("org.primefaces:primefaces:2.2.1")),
             2),
-    PRIMEFACES_3_0_RC2_SNAPSHOT("Primefaces 3.0.RC2",
-            Arrays.asList(DependencyBuilder.create("org.primefaces:primefaces:3.0.RC2")), 3);
+    PRIMEFACES_3_0("Primefaces 3.0",
+            Arrays.asList(DependencyBuilder.create("org.primefaces:primefaces:3.0")), 3);
 
     private List<? extends Dependency> dependencies;
 
